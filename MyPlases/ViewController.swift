979bec0f735492cab1b37ad075d769db005af1f5
Sergey <e853ex@yandex.ru>
2020-07-17 12:47:05 +0300

    Initial Commit

---
+++ MyPlases/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlases
+//
+//  Created by Сергей Васильев on 17.07.2020.
+//  Copyright © 2020 Сергей Васильев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

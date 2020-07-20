cd62b8695c4acaa299cc25f570e5e56ec7ec65d4
Sergey <e853ex@yandex.ru>
2020-07-20 16:00:50 +0300

    Custom Table

---
+++ MyPlases/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlases
+//
+//  Created by Сергей Васильев on 20.07.2020.
+//  Copyright © 2020 Сергей Васильев. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlase: UIImageView!
+    @IBOutlet weak var nameLable: UILabel!
+    @IBOutlet weak var lacationLable: UILabel!
+    @IBOutlet weak var typeLable: UILabel!
+    
+}
